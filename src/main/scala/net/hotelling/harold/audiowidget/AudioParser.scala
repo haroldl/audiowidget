5866072bc6ba8d0f2d67fa7631ebfec407422bfb
Harold Lee <harold@hotelling.net>
2017-01-16 11:19:06 -0800

    Refactor the AudioWidget to push all audio-processing logic into AudioParser.

--- src/main/scala/net/hotelling/harold/audiowidget/AudioParser.scala
+++ src/main/scala/net/hotelling/harold/audiowidget/AudioParser.scala
@@ -1,6 +1,7 @@
 package net.hotelling.harold.audiowidget
 
-import javax.sound.sampled.{AudioFormat, TargetDataLine}
+import javax.sound.sampled.DataLine.Info
+import javax.sound.sampled._
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -52,5 +53,72 @@ class AudioParser(val format: AudioFormat, val timeWindowMillis: Double = 50.0) 
       throw new RuntimeException(s"weird sized audio input frames are not supported... $format")
     }
     result.toArray
+  }
+}
+
+object AudioParser {
+  def getMixerByName(name: String = "Primary Sound Capture Driver"): Mixer = {
+    val mixerInfo = AudioSystem.getMixerInfo.filter { _.getName == name }.head
+    println("Found mixer info: " + mixerInfo)
+    AudioSystem.getMixer(mixerInfo)
+  }
+
+  def getSomeInputLine(mixer: Mixer): TargetDataLine = {
+    val line = mixer.getTargetLineInfo.map(mixer.getLine)
+      .filter(_.isInstanceOf[TargetDataLine]).map(_.asInstanceOf[TargetDataLine]).head
+    line.open()
+    line
+  }
+
+  def getLineFormats(line: TargetDataLine): Array[AudioFormat] = {
+    line.getLineInfo.asInstanceOf[DataLine.Info].getFormats
+  }
+
+  def dumpInfo(): Unit = {
+    val mixers = AudioSystem.getMixerInfo
+    mixers.foreach { mixerInfo =>
+      println(mixerInfo.getName)
+      println(mixerInfo.getDescription)
+      println(mixerInfo.getVendor)
+      println(mixerInfo.getVersion)
+
+      val mixer: Mixer = AudioSystem.getMixer(mixerInfo)
+      mixer.getSourceLineInfo.foreach { sourceLineInfo =>
+        println(s"  $sourceLineInfo")
+        val line = mixer.getLine(sourceLineInfo)
+
+        line match {
+          case sdl: SourceDataLine =>
+            sdl.getLineInfo.asInstanceOf[Info].getFormats.foreach { format =>
+              println(s"    output: $format")
+            }
+          case _ =>
+        }
+      }
+      mixer.getTargetLineInfo.foreach { targetLineInfo =>
+        println(s"  $targetLineInfo")
+
+        val line = mixer.getLine(targetLineInfo)
+        line match {
+          case tdl: TargetDataLine =>
+            tdl.getLineInfo.asInstanceOf[Info].getFormats.foreach { format =>
+              println(s"    input: $format")
+            }
+          case _ =>
+        }
+      }
+
+      println()
+
+      val targetLines = mixer.getTargetLines
+      targetLines.foreach { targetLine => println(targetLine.getLineInfo) }
+      println(s"targetLines: ${targetLines.size}")
+      println()
+    }
+
+    val mixer = AudioSystem.getMixer(mixers(0))
+    val targetLines = mixer.getTargetLines
+    targetLines.foreach { targetLine => println(targetLine.getLineInfo) }
+    println(s"targetLines: ${targetLines.size}")
   }
 }
